1a85cbb79acf7209cc1e25b971fd6a34dfecb010
Casey Bruce <cbruce1024@gmail.com>
2023-10-16 23:34:51 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+# 1)
+# user_first_name = input("What is your first name? ")
+# user_last_name = input("What is your last name? ")
+#
+# print(user_last_name + " " + user_first_name)
+
+
+# 2)
+# user_num_pick = input("Pick a number between 1-10: ")
+#
+# n_1 = user_num_pick
+# n_2 = user_num_pick.join(user_num_pick)
+# n_3 = n_2.join(user_num_pick)
+#
+# print(n_1 + n_2 + n_3)
+
+# 3)
+
+# user_country = input("What Country are you from? ")
+#
+# print(f"I have heard that {user_country} is a beautiful country!")
+
+# 4)
+
+# x = 10
+# y = 50
+# if x ** 2 > 100 and y < 100:
+#     print(x, y)
+# There is no output- the conditions of the if statement are not met. Thus, the print statement isn't executed
+
+# 5)
+# a = [10, 20]
+# b = a
+# b += [30, 40]
+# print(a)
+# print(b)
+# the += operator both adds and assigns values. It concatenates the [10, 20] list with the [30,40] list
+
+# 6)
+# print(2%6)
+# The above code prints 2 in the console
+# This is because the % is the modulo operator that checks for remainders.
+# Thus, the remainder when 2 is divided by 6 is 2
+
+# 7)
+# print(2 * 3 ** 3 * 4)
+# The output is 216
+# This is because the computer is following order of operations and the exponent (**) operation is done first.
+
+# 8)
+# A text editor is an environment that allows user to create text files, especially code-based files that are used by /
+# programmers to write and edit code
+
+# 9)
+# Python is an interpreted programming language that has an easy to read syntax (No semicolons!) and a large community /
+# with many libraries and packages
+# Fun fact: Python was name after the British Comedy group Monty Python
+
+# 10)
+# Jupyter is a web-based development environment or IDE that allows users to write and edit code
+# Alternatives to Jupyter include Spyder, VSCode, and PyCharm
+
+
+
+
+
+
+
